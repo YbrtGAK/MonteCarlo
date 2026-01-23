e0039670de2092b3382316a678584abac4b7a6b5
Yann <yann.berton@yahoo.com>
2026-01-23 13:46:52 +0100

    Final fix of ProbabilisticDataFrame  : - No subcases for pd.Series anymore - Found a way to compute CoolProp functions by vectorization of lambda functions

--- MC_Mistral.py
+++ MC_Mistral.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from CoolProp.CoolProp import PropsSI
+from utilities.data.lvm import lvm_to_df
 
 # Définition de la fonction H
 H = lambda P, T: PropsSI('H', 'P', P*1E5, 'T', T+273.15, 'R245fa')
@@ -14,4 +15,5 @@ df.drop(columns=["Comment"],inplace=True)
 H_vectorized = np.vectorize(H)
 
 # Application de H_vectorized sur chaque élément des listes de A et B
-df['C'] = [H_vectorized(a, b).tolist() for a, b in zip(df['A'], df['B'])]
+df['H'] = [H_vectorized(a, b).tolist() for a, b in zip(df['118 - P_TS_in [bars]'], df['202 - E_in_imm [°C]'])]
+print(f"df['H'] = {df['H']}")
--- ProbabilisticDataFrame.py
+++ ProbabilisticDataFrame.py
@@ -11,8 +11,7 @@ import pandas as pd
 from scipy.stats import chi2 
 import numpy as np
 from CoolProp.CoolProp import PropsSI
-
-H = lambda P,T : PropsSI('H', 'P', P*1E5, 'T', T+273.15, 'R245fa')
+H = np.vectorize(lambda P,T : PropsSI('H', 'P', P*1E5, 'T', T+273.15, 'R245fa'))
 np.random.seed(42) # Seed fixed for random sample generations
 
 # Class definition
@@ -54,37 +53,50 @@ class ProbabilisticDataFrame():
         """Allow the user to calculate the expression exp and the uncertainty over
         the output value with Monte Carlo Simulation"""
 
+        # Determination of N_pop for a Serie or a DataFrame
+
+        if len(self.df.shape) == 2 : 
+            N_pop = self.df.shape[0]
+        else :
+            N_pop = 1
+
+        # Test user input : Nb var input has to be equal to the nb var in the expression
+
         if len([l for l in exp[-1].split('X')[1:] if l[0].isdigit()]) != len(Xk) :
             raise ValueError("""
             Xk and exp have different variable numbers.
             If not, make sure there is no Xi with i a number 
             that is not a variable in your expression.""")
-        self.exp = exp #### A RETIRER
+        
         N = self.N_samples_calculation(alpha) # Determination of the number of simulation samples N
         nVar = len(Xk)
         exp_df = exp_udf = exp_df_mc = exp[-1] # Initialize expressions for df and udf
 
-        df_mc = pd.DataFrame(index=range(len(self.df)), columns=Xk)
+        # Initialization of df_mc with  index == df.index and columns == nVar 
+        df_mc = pd.DataFrame(index=range(N_pop), columns=Xk)
+        # Replace each input variable in the expression
         for i in range(nVar) :
-            exp_df = exp_df.replace('X'+str(i), "self.df['" + Xk[i] + "'].values")
-            exp_udf = exp_udf.replace('X'+str(i), "self.udf['" + Xk[i] + "'].values")
-            exp_df_mc = exp_df_mc.replace('X'+str(i), "df_mc['" + Xk[i] + "'].values")
-
-            samples = np.random.normal(loc = self.df[Xk[i]].values,
-                                    scale = self.udf[Xk[i]].values,
-                                    size = (N,len(self.df)))
-            
+            # Expression for df
+            exp_df = exp_df.replace('X'+str(i), "self.df['" + Xk[i] + "']")
+            # Expression to calculate the error
+            exp_udf = exp_udf.replace('X'+str(i), "self.udf['" + Xk[i] + "']")
+            # Expression to apply the expression on the samples in df_mc
+            exp_df_mc = exp_df_mc.replace('X'+str(i), "df_mc['" + Xk[i] + "']")
+            # Generation of the samples for each variable
+            samples = np.random.normal(loc = self.df[Xk[i]],
+                                    scale = self.udf[Xk[i]],
+                                    size = (N,N_pop))
+            # Shaping the list to be added to df_mc
             list_samples = [samples[:,index] for index in range(len(samples[0]))]
-            
+            # Add the new column with generated samples for each 
             df_mc[Xk[i]] = pd.Series(list_samples)
-                ## Generate random samples for each Xk and calculate their mean and std
 
-        # Monte Carlo simulations for uncertainty calculation
+        ## Monte Carlo simulations for uncertainty calculation
 
-        ## Random population initialization for the variables Xk
-
-        ## Evaluate the expression with df_mc
+        # Evaluate the expression with df_mc
         df_mc[exp[0]] = eval(exp_df_mc)
+
+        # Calculate their mean and std
         self.df[exp[0]] = df_mc[exp[0]].apply(np.mean)
         self.udf[exp[0]] = df_mc[exp[0]].apply(np.std)
 
@@ -102,31 +114,19 @@ class ProbabilisticDataFrame():
             diff = (chi2.ppf((1-alpha/2),nu) - chi2.ppf(alpha/2,nu))/nu
         N = (nu + 1)*2 # Overestimation of N to make sure of the independance of the samples
         return(N)
-    
-    def add_functions(self,**kwargs):
-        print(type(**kwargs))
 
 if __name__ == "__main__" :
 
     """Quick test to use the probilistic DataFrame"""
 
-    data = {'A' : [1,2], 'B' : [3,4]}
-    udata = {'A' : [0.01,0.02], 'B' : [0.03,0.04]}
+    data = {'A' : [1,2,3], 'B' : [4,5,6]}
+    udata = {'A' : [0.01,0.02,0.03], 'B' : [0.04,0.05,0.06]}
     df = pd.DataFrame(data)
     udf = pd.DataFrame(udata)
+    # pdf = ProbabilisticDataFrame(df.mean(),udf.mean())
     pdf = ProbabilisticDataFrame(df,udf)
-    exp = "H(X0,H1)"
-    pdf.propagate(['A','B'],exp=["C",exp],alpha=0.05)
-
-    """Quick test to use the probilistic Serie"""       
+    H = np.vectorize(lambda P,T : PropsSI('H', 'P', P*1E5, 'T', T+273.15, 'R245fa'))
+    exp = "[H(a, b).tolist() for a, b in zip(X0,X1)]"
+    pdf.propagate(['A','B'],exp=["H",exp],alpha=0.05)
+    print(pdf.df['H'])
    
-   
-"""
-data = {'A' : [1,2], 'B' : [3,4]}
-udata = {'A' : [0.01,0.02], 'B' : [0.03,0.04]}
-df = pd.DataFrame(data).mean()
-udf = pd.DataFrame(udata).std()
-pdf = ProbabilisticDataFrame(df,udf)
-pdf.propagate(['A','B'],exp=["C","X0*X1"],alpha=0.05)
-    
-"""
